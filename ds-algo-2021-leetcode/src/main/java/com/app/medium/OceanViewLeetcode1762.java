e2d57218088864c25780f2ef5df4290dea4964da
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2022-03-12 09:12:37 +0530

    leetcode

--- ds-algo-2021-leetcode/src/main/java/com/app/medium/OceanViewLeetcode1762.java
+++ ds-algo-2021-leetcode/src/main/java/com/app/medium/OceanViewLeetcode1762.java
@@ -1,5 +1,38 @@
-package com.app.medium;/**
- * @project ds-algo-2021
+package com.app.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
  * @author t0k02w6 on 11/03/22
- */public class OceanViewLeetcode1762 {
+ * @project ds-algo-2021
+ */
+public class OceanViewLeetcode1762 {
+    private static int[] findBuildings(int[] heights) {
+       Stack<Integer> stack = new Stack<>();
+       int n = heights.length;
+       stack.push((n - 1));
+       int max = heights[n - 1];
+
+       for(int i = n - 2; i >= 0; i--) {
+           if(heights[i] > max) {
+               max = heights[i];
+               stack.push(i);
+           }
+       }
+
+       int[] result = new int[stack.size()];
+       int i = 0;
+       while(!stack.isEmpty()) {
+           result[i++] = stack.pop();
+       }
+       return result;
+    }
+
+    public static void main(String[] args) {
+        int[] heights = {4,2,3,1};
+
+        System.out.println(Arrays.toString(findBuildings(heights)));
+    }
 }
--- ds-algo-2021-leetcode/src/main/java/com/app/medium/SubdomainVisitCount.java
+++ ds-algo-2021-leetcode/src/main/java/com/app/medium/SubdomainVisitCount.java
@@ -1,5 +1,54 @@
-package com.app.medium;/**
- * @project ds-algo-2021
+package com.app.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
  * @author t0k02w6 on 11/03/22
- */public class SubdomainVisitCount {
+ * @project ds-algo-2021
+ */
+public class SubdomainVisitCount {
+    private static void addData(Map<String, Integer> map, String key, Integer value) {
+        if(!map.containsKey(key)) {
+            map.put(key, 0);
+        }
+        int val = map.getOrDefault(key, 0);
+        val += value;
+        map.put(key, val);
+    }
+
+    private static List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> domainMap = new HashMap<>();
+        for(String domain: cpdomains) {
+            String[] domainTokens = domain.split("\\s+");
+            Integer count = Integer.parseInt(domainTokens[0].trim());
+            String[] domainValues = domainTokens[1].split("\\.");
+            String ss = domainValues[domainValues.length - 1];
+            for(int i = domainValues.length - 2; i >= 0; i--) {
+                if(!ss.isEmpty()) {
+                    addData(domainMap, ss, count);
+                }
+                ss = domainValues[i] + "." + ss;
+            }
+            addData(domainMap, ss, count);
+        }
+        List<String> res = new ArrayList<>();
+        for(Map.Entry<String, Integer> entry: domainMap.entrySet()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(entry.getValue());
+            sb.append(" ");
+            sb.append(entry.getKey());
+            res.add(sb.toString());
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String[] str = {"9001 discuss.leetcode.com"};
+
+        List<String> res = subdomainVisits(str);
+        System.out.println(res);
+    }
 }

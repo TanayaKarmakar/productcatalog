ebc1346f7011a04f3e21cd5e9c49cf6aa5169439
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2022-03-20 09:24:30 +0530

    code

--- pepcoding/src/main/java/recursion/Power.java
+++ pepcoding/src/main/java/recursion/Power.java
@@ -1,5 +1,26 @@
-package recursion;/**
- * @project ds-algo-2021
+package recursion;
+
+import java.util.Scanner;
+
+/**
  * @author t0k02w6 on 19/03/22
- */public class Power {
+ * @project ds-algo-2021
+ */
+public class Power {
+    private static int power(int x, int n) {
+        if(n == 0)
+            return 1;
+        return x * power(x, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        int ans = power(x, n);
+
+        System.out.println(ans);
+
+    }
 }
--- pepcoding/src/main/java/recursion/PowerLogarithmic.java
+++ pepcoding/src/main/java/recursion/PowerLogarithmic.java
@@ -1,5 +1,29 @@
-package recursion;/**
- * @project ds-algo-2021
+package recursion;
+
+import java.util.Scanner;
+
+/**
  * @author t0k02w6 on 19/03/22
- */public class PowerLogarithmic {
+ * @project ds-algo-2021
+ */
+public class PowerLogarithmic {
+    private static int power(int x, int n) {
+        if(n == 0)
+            return 1;
+        int smallerValue = power(x, n / 2);
+        int value = smallerValue * smallerValue;
+        if(n % 2 != 0) {
+            value = x * value;
+        }
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int n = scanner.nextInt();
+        int ans = power(x, n);
+        System.out.println("Ans - " + ans);
+        scanner.close();
+    }
 }

29d1a380f8b9c1eea6171a22618a814ce4f2bb56
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2022-03-25 09:38:43 +0530

    code

--- ds-algo-2021-leetcode/src/main/java/com/app/common/Pair.java
+++ ds-algo-2021-leetcode/src/main/java/com/app/common/Pair.java
@@ -1,5 +1,15 @@
-package com.app.common;/**
- * @project ds-algo-2021
+package com.app.common;
+
+/**
  * @author t0k02w6 on 21/03/22
- */public class Pair {
+ * @project ds-algo-2021
+ */
+public class Pair<S, T> {
+    public S first;
+    public T second;
+
+    public Pair(S first, T second) {
+        this.first = first;
+        this.second = second;
+    }
 }
--- ds-algo-2021-leetcode/src/main/java/com/app/common/Triplet.java
+++ ds-algo-2021-leetcode/src/main/java/com/app/common/Triplet.java
@@ -1,5 +1,41 @@
-package com.app.common;/**
- * @project ds-algo-2021
+package com.app.common;
+
+/**
  * @author t0k02w6 on 25/03/22
- */public class Triplet {
+ * @project ds-algo-2021
+ */
+public class Triplet<S, T, U> {
+    private S first;
+    private T second;
+    private U third;
+
+    public Triplet(S first, T second, U third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public S getFirst() {
+        return first;
+    }
+
+    public void setFirst(S first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+
+    public U getThird() {
+        return third;
+    }
+
+    public void setThird(U third) {
+        this.third = third;
+    }
 }
